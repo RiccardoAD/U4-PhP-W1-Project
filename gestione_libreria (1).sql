ce94faacee609803561fb4069248783e33a8ba8e
RiccardoAD <rick_95@libero.it>
2024-04-15 16:40:13 +0200

    fix

--- gestione_libreria (1).sql
+++ gestione_libreria (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2024 at 04:35 PM
+-- Generation Time: Apr 15, 2024 at 04:39 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `gestione_libreria`
 --
+CREATE DATABASE IF NOT EXISTS `gestione_libreria` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `gestione_libreria`;
 
 -- --------------------------------------------------------
 
