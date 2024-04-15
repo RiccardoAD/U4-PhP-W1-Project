24905ccb99df959ab0fa93bb2c72ad4e9517834c
RiccardoAD <rick_95@libero.it>
2024-04-15 16:36:07 +0200

    database e non tabella

--- gestione_libreria.sql
+++ gestione_libreria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2024 at 04:29 PM
+-- Generation Time: Apr 15, 2024 at 04:35 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
